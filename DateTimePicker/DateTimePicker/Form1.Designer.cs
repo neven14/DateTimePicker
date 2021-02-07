774208db345a33750f73dc37a658f1ee71cc0b46
Neven <58472164+neven14@users.noreply.github.com>
2021-02-07 15:34:01 +0100

    Add files via upload

---
+++ DateTimePicker/DateTimePicker/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace DateTimePicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePickerDatum = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePickerVrijeme = new System.Windows.Forms.DateTimePicker();
+            this.btnRezervacija = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dateTimePickerDatum
+            // 
+            this.dateTimePickerDatum.Location = new System.Drawing.Point(497, 12);
+            this.dateTimePickerDatum.Name = "dateTimePickerDatum";
+            this.dateTimePickerDatum.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePickerDatum.TabIndex = 0;
+            // 
+            // dateTimePickerVrijeme
+            // 
+            this.dateTimePickerVrijeme.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePickerVrijeme.Location = new System.Drawing.Point(497, 79);
+            this.dateTimePickerVrijeme.Name = "dateTimePickerVrijeme";
+            this.dateTimePickerVrijeme.ShowUpDown = true;
+            this.dateTimePickerVrijeme.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePickerVrijeme.TabIndex = 1;
+            this.dateTimePickerVrijeme.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
+            // 
+            // btnRezervacija
+            // 
+            this.btnRezervacija.Location = new System.Drawing.Point(516, 235);
+            this.btnRezervacija.Name = "btnRezervacija";
+            this.btnRezervacija.Size = new System.Drawing.Size(167, 89);
+            this.btnRezervacija.TabIndex = 2;
+            this.btnRezervacija.Text = "Rezervacija";
+            this.btnRezervacija.UseVisualStyleBackColor = true;
+            this.btnRezervacija.Click += new System.EventHandler(this.btnRezervacija_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(94, 171);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(342, 237);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(296, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Upišite datum leta";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(296, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Upišite vrijeme leta";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnRezervacija);
+            this.Controls.Add(this.dateTimePickerVrijeme);
+            this.Controls.Add(this.dateTimePickerDatum);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePickerDatum;
+        private System.Windows.Forms.DateTimePicker dateTimePickerVrijeme;
+        private System.Windows.Forms.Button btnRezervacija;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ DateTimePicker/DateTimePicker/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DateTimePicker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRezervacija_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            DateTime dt1 = dateTimePickerDatum.Value;
+            StringBuilder sb1 = new StringBuilder();
+            sb1.Append("Vas datum leta je:\n");
+            sb1.Append(dt1.ToShortDateString() + "\n");
+            DateTime dt2 = dateTimePickerVrijeme.Value;
+            sb1.Append("Vase vrijeme polaska je:\n");
+            sb1.Append(dt2.ToShortTimeString());
+            richTextBox1.AppendText(sb1.ToString());
+        }
+    }
+}
